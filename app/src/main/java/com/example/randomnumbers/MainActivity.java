3d909271021677942d5667ba27d519a8991fadef
Weebi <WeebiDaBoi@gmail.com>
2022-05-10 18:42:33 -0400

    Main code upload

---
+++ app/src/main/java/com/example/randomnumbers/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.randomnumbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
